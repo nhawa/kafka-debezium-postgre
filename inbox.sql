c044d67523d6576cacfa1a790e0d895dabd77611
anwar sandi <anwar.sandi@ottencoffee.co.id>
2024-10-29 14:42:26 +0700

    update

---
+++ inbox.sql
@@ -0,0 +1,10 @@
+CREATE TABLE inbox (
+	id serial NOT NULL,
+	payload text,
+	send_at timestamp NOT NULL,
+	sent_at timestamp NOT NULL,
+	is_ready bool NOT null default false,
+	CONSTRAINT inbox_pkey PRIMARY KEY (id)
+);
+
+select * from inbox;
